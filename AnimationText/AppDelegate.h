dc9f76c7d550095040f56705f79ca3071d4f6bfb
liyang_a <771510512@qq.com>
2016-04-11 18:09:03 +0800

    commit init

---
+++ AnimationText/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AnimationText
+//
+//  Created by 李阳 on 16/4/11.
+//  Copyright © 2016年 liyang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AnimationText/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AnimationText
+//
+//  Created by 李阳 on 16/4/11.
+//  Copyright © 2016年 liyang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
